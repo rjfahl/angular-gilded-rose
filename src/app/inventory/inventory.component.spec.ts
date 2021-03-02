17a67baec747760d193a0170405abcf95dc5d136
Robert Fahl <rfahl@apterainc.com>
2021-03-01 19:29:25 -0500

    Code dojo results

--- src/app/inventory/inventory.component.spec.ts
+++ src/app/inventory/inventory.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { InventoryComponent } from './inventory.component';
 
-
 let inventory: InventoryComponent;
 
 describe('The Gilded Rose', () => {
@@ -12,15 +11,86 @@ describe('The Gilded Rose', () => {
     expect(inventory).toBeTruthy();
   });
 
-  it("should foo", function() {
-    inventory.items = [{name: "", sellIn: 0, quality: 0}];
-    inventory.updateQuality();
-    expect(inventory.items[0].name).toEqual("fixme");
+  it('inverntory items should decrease in quality', function () {
+    updateItemQuality('', 30, 10);
+    expect(inventory.items[0].quality).toEqual(9);
+  });
+
+  it('inventory items should decrease in quality twice as fast after sell-in date', function () {
+    updateItemQuality('', 0, 10);
+    expect(inventory.items[0].quality).toEqual(8);
+  });
+
+  it('inventory items should never have a negative quality value', function () {
+    updateItemQuality('', 0, 0);
+    expect(inventory.items[0].quality).toEqual(0);
+  });
+
+  it('Aged Brie should increase in quality over time', function () {
+    updateItemQuality('Aged Brie', 1, 10);
+    expect(inventory.items[0].quality).toEqual(11);
+  });
+
+  it('Aged Brie should increase in quality twice as fast after sell-in date', function () {
+    updateItemQuality('Aged Brie', 0, 10);
+    expect(inventory.items[0].quality).toEqual(12);
+  });
+
+
+  it('Aged brie quality should not exceed 50', function () {
+    updateItemQuality('Aged Brie',1 ,50);
+    expect(inventory.items[0].quality).toEqual(50);
+  });
+
+  it('Item quality should reduce by one, or continue to degrade if initialized with quality above 50', function () {
+    updateItemQuality('',1 ,52);
+    expect(inventory.items[0].quality).toEqual(51);
+  });
+  
+  it('Legendary items does not degrade', function () {
+    updateItemQuality('Sulfuras, Hand of Ragnaros',1 ,40);
+    expect(inventory.items[0].quality).toEqual(40);
+  });
+
+  it('Legendary items does not have to be sold', function () {
+    updateItemQuality('Sulfuras, Hand of Ragnaros', 1,40);
+    expect(inventory.items[0].sellIn).toEqual(1);
+  });
+
+  it('Backstage passes value should increase in value as sell-in date approaches', function () {
+    updateItemQuality('Backstage passes to a TAFKAL80ETC concert', 15, 10);
+    expect(inventory.items[0].quality).toEqual(11);
+  });
+
+  it('Backstage passes value should increase in value by 2 if sell-in date is between 10-6', function () {
+    updateItemQuality('Backstage passes to a TAFKAL80ETC concert', 10, 10);
+    expect(inventory.items[0].quality).toEqual(12);
+  });
+
+  it('Backstage passes value should increase in value by 3 if sell-in date is between 5-1', function () {
+    updateItemQuality('Backstage passes to a TAFKAL80ETC concert', 1, 10);
+    expect(inventory.items[0].quality).toEqual(13);
+  });
+
+  it('Backstage passes value should be zero if sell-in date is 0 or less', function () {
+    updateItemQuality('Backstage passes to a TAFKAL80ETC concert', 0, 10);
+    expect(inventory.items[0].quality).toEqual(0);
+  });
+
+  it('Conjured items degrade in Quality twice as fast as normal items', function () {
+    updateItemQuality('Conjured Healthstones', 1, 10);
+    expect(inventory.items[0].quality).toEqual(8);
   });
 });
 
+
+function updateItemQuality(name: string, sellIn: number, quality: number) {
+  inventory.items = [{ name: name, sellIn: sellIn, quality: quality}];
+  inventory.updateQuality();
+}
+
 async function arrange() {
   inventory = await TestBed.configureTestingModule({
-    declarations: [ InventoryComponent ],
+    declarations: [InventoryComponent],
   }).createComponent(InventoryComponent).componentInstance;
 }
--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 interface ItemInterface {
   name: string;
   sellIn: number;
-  quality: number
+  quality: number;
 }
 
 class Item implements ItemInterface {
@@ -11,7 +11,7 @@ class Item implements ItemInterface {
   sellIn: number = 0;
   quality: number = 0;
 
-  constructor(name: string, sellIn: number, quality: number){
+  constructor(name: string, sellIn: number, quality: number) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -21,66 +21,85 @@ class Item implements ItemInterface {
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: ['./inventory.component.scss'],
 })
 export class InventoryComponent {
   items: Array<Item> = [
-    new Item("Aged Brie", 20, 10),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 20, 15),
-    new Item("Sulfuras, Hand of Ragnaros", 30, 48),
-    new Item("Soap On A Rope", 30, 25),
-    new Item("103 Pound Mightfish", 30, 4),
+    new Item('Aged Brie', 20, 10),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 20, 15),
+    new Item('Sulfuras, Hand of Ragnaros', 30, 48),
+    new Item('Soap On A Rope', 30, 25),
+    new Item('103 Pound Mightfish', 30, 4),
     new Item("Belbi's Eyesight Enhancing Romance Goggles", 30, 1),
-    new Item("Conjured Healthstones", 30, 20) // new conjured item - not updating properly
+    new Item('Conjured Healthstones', 30, 20), // new conjured item - not updating properly
   ];
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+      this.updateQualityPositiveSellIn(i);
+
+      this.updateSellInNonLegendary(i);
+
+      this.updateQualityNegativeSellIn(i);
+    }
+
+    return this.items;
+  }
+
+  private updateQualityNegativeSellIn(i: number) {
+    if (this.items[i].sellIn < 0) {
+      if (this.items[i].name != 'Aged Brie') {
+        if (this.isDefaultItem(i)) {
+          this.items[i].quality = this.items[i].quality - 1;
+        } else {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
         }
       }
     }
+  }
 
-    return this.items;
+  private isDefaultItem(i: number) {
+    return this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros';
+  }
+
+  private updateSellInNonLegendary(i: number) {
+    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      this.items[i].sellIn = this.items[i].sellIn - 1;
+    }
+  }
+
+  private updateQualityPositiveSellIn(i: number) {
+    if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      this.decreasingItemQuality(i);
+    } else {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          this.updateBackstagePasses(i);
+        }
+      }
+    }
+  }
+
+  private decreasingItemQuality(i: number) {
+    if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Conjured Healthstones') {
+      this.items[i].quality = this.items[i].quality - 1;
+    }
+    else if (this.items[i].name == 'Conjured Healthstones') {
+      this.items[i].quality = this.items[i].quality - 2;
+    }
+  }
+
+  private updateBackstagePasses(i: number) {
+    if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1;
+    }
+    if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1;
+    }
   }
 }
